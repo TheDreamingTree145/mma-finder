9fb390c0ec528516a445b314a179cde3b2bc392f
dustin <dandersonsoftware@outlook.com>
2017-11-08 12:25:04 -0500

    removed store for time being

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './configureStore';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />,
-    document.getElementById('root')
-  </Provider>
+  <App />,
+  document.getElementById('root')
 );
 registerServiceWorker();
