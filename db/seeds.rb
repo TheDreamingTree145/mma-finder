a6a0940ceffea960f8f221564009bad87785b349
dustin <dandersonsoftware@outlook.com>
2017-11-08 01:33:57 -0500

    added custom header

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+fighter_1 = Fighter.create(name: "Jon Jones", age: 28, weight_class: "Light Heavyweight", hometown: "Buffalo")
