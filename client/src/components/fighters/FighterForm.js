6beb7179fcabea4019711e84c49b10754609d21a
dustin <dandersonsoftware@outlook.com>
2017-11-12 22:32:45 -0500

    setup new fighter form. no formatting and not working

--- client/src/components/fighters/FighterForm.js
+++ client/src/components/fighters/FighterForm.js
@@ -16,8 +16,29 @@ class FighterForm extends Component {
   render() {
 
     return (
-
-      <h1>New Fighter Form</h1>
+      <div className="fightersForm">
+        <h1>New Fighter Form</h1>
+          <form>
+            <label>Name: </label>
+            <input type='text' id="fighterName" /><br /><br />
+            <label>Age: </label>
+            <input type='number' min='18' max='99' /><br /><br />
+            <label>Weight Class </label>
+            <select>
+              <option value="Flyweight">Flyweight</option>
+              <option value="Bantamweight">Bantamweight</option>
+              <option value="Featherweight">Featherweight</option>
+              <option value="Lightweight">Lightweight</option>
+              <option value="Welterweight">Welterweight</option>
+              <option value="Middleweight">Middleweight</option>
+              <option value="Light Heavyweight">Light Heavyweight</option>
+              <option value="Heavyweight">Heavyweight</option>
+            </select><br /><br />
+            <label>Hometown </label>
+            <input type="text" id="fighterHometown" /><br /><br />
+            <input type="submit" value="Submit New Fighter" />
+          </form>
+      </div>
     )
   }
 }
