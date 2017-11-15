4394729fc8cfa43e3977b8988d16c8b384e8d8a2
dustin <dandersonsoftware@outlook.com>
2017-11-15 05:01:54 -0500

    updated fighter details page

--- client/src/components/fighters/FighterDetails.js
+++ client/src/components/fighters/FighterDetails.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const FighterDetails = (props) => {
-  debugger;
   return (
     <div>
       <h1>{props.fighter.name}</h1>
@@ -10,7 +9,7 @@ const FighterDetails = (props) => {
         <p>Age: {props.fighter.age}</p>
         <p>Weight Class: {props.fighter.weight_class}</p>
         <p>Hometown: {props.fighter.hometown}</p>
-        <Link style={{ marginRight: '12px' }} key={props.fighter.id} to={`/fighters/${props.fighter.gym_id}`}>{props.fighter.name}</Link>
+        <span>Gym: <Link style={{ marginRight: '12px' }} key={props.fighter.id} to={`/fighters/${props.fighter.gym.id}`}>{props.fighter.gym.name}</Link></span>
       </ul>
     </div>
   )
