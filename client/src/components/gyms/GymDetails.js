cdd5c9a31ba236e3c573e3b1622e2cba8269000a
dustin <dandersonsoftware@outlook.com>
2017-11-16 05:45:07 -0500

    added semantic to fighter show page

--- client/src/components/gyms/GymDetails.js
+++ client/src/components/gyms/GymDetails.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 const GymDetails = (props) => {
   return (
-    <div>
-      <h1>{props.gym.name}</h1>
-      <strong>Location: {props.gym.location}</strong><br />
-      <strong>Owner: {props.gym.owner}</strong>
+    <div className="ui internally celled grid container">
+      <div className="row">
+        <div className="four wide column">
+          <h1>{props.gym.name}</h1>
+          <strong>Location: {props.gym.location}</strong><br />
+          <strong>Owner: {props.gym.owner}</strong>
+        </div>
+        <div className="six wide column">
+          <p>Description: PENDING</p>
+        </div>
+        <div className="four wide column">
+          <h2>Pending Image</h2>
+        </div>
+      </div>
     </div>
   )
 }
