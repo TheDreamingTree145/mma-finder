a382893838d5659845ac932a96ec85920b5ef432
dustin <dandersonsoftware@outlook.com>
2017-11-16 03:46:52 -0500

    filled out html for new gym form

--- client/src/components/gyms/GymForm.js
+++ client/src/components/gyms/GymForm.js
@@ -6,8 +6,29 @@ class GymForm extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="gymForm">
+        <h1>New Gym Form</h1>
+          <form>
+            <label>Gym Name: </label>
+            <input
+            type="text"
+            name="gymName"
+            /><br /><br />
+
+            <label>Gym Location: </label>
+            <input
+            type="text"
+            name="gymLocation"
+            /><br /><br />
+
+            <label>Gym Owner: </label>
+            <input
+            type="text"
+            name="gymOwner"
+            /><br /><br />
+
+            <input type="submit" value="Submit New Gym" />
+          </form>
       </div>
     )
   }
