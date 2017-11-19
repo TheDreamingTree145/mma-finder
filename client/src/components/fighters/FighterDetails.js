a2d906c94a6088b28acbcfbff1ca85fc11d158c9
dustin <dandersonsoftware@outlook.com>
2017-11-19 04:52:17 -0500

    routes and props setup for fighter edit

--- client/src/components/fighters/FighterDetails.js
+++ client/src/components/fighters/FighterDetails.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import FighterEdit from './FighterEdit'
 
-const FighterDetails = ({fighterActions}) => {
+const FighterDetails = ({fighter}) => {
 
-  debugger;
-  
   const WrappedLink = () => {
+    debugger;
     return (
       <button>
-        <Link style={{ marginBottom: '5px'}} to={`/fighters/${props.fighter.id}`}>Edit this Fighter</Link>
+        <Link style={{ marginBottom: '5px'}} to={`/fighters/${fighter.id}/edit`}>Edit this Fighter</Link>
       </button>
     )
   }
@@ -20,22 +19,22 @@ const FighterDetails = ({fighterActions}) => {
         <div className="four wide column">
           <h1>{fighter.name}</h1>
           <p>Age: {fighter.age}</p>
-          <p>Weight Class: {props.fighter.weight_class}</p>
-          <p>Hometown: {props.fighter.hometown}</p>
-          <p>Gym: <Link style={{ marginBottom: '5px'}} to={`/gyms/${props.fighter.gym.id}`}>{props.fighter.gym.name}</Link></p>
+          <p>Weight Class: {fighter.weight_class}</p>
+          <p>Hometown: {fighter.hometown}</p>
+          <p>Gym: <Link style={{ marginBottom: '5px'}} to={`/gyms/${fighter.gym.id}`}>{fighter.gym.name}</Link></p>
         </div>
         <div className="six wide column">
-          <p>{props.fighter.description}</p>
+          <p>{fighter.description}</p>
         </div>
         <div className="four wide column">
-          <img src={props.fighter.image_url} />
+          <img src={fighter.image_url} />
         </div>
         <div>
-          <WrappedLink props={props} />
+          <WrappedLink />
         </div>
         <div>
           <Switch>
-            <Route exact path={`${match.url}/:fighterId/edit`} component={(props) => <FighterEdit {...props} fighter={fighter} />}  />
+            <Route exact path={`/fighters/:fighterId/edit`} component={(props) => <FighterEdit {...props} fighter={fighter} />} />
           </Switch>
         </div>
       </div>
