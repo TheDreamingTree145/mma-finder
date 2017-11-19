b810708b5eed484a0d16e7c3242f7dbaaa127c56
dustin <dandersonsoftware@outlook.com>
2017-11-19 01:56:35 -0500

    began routing and setup for fighter edit

--- client/src/components/fighters/FighterDetails.js
+++ client/src/components/fighters/FighterDetails.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
+import FighterEdit from './FighterEdit'
 
-const FighterDetails = (props) => {
+const FighterDetails = ({fighterActions}) => {
+
+  debugger;
+  
+  const WrappedLink = () => {
+    return (
+      <button>
+        <Link style={{ marginBottom: '5px'}} to={`/fighters/${props.fighter.id}`}>Edit this Fighter</Link>
+      </button>
+    )
+  }
+
   return (
     <div className="ui internally celled grid container">
       <div className="row">
         <div className="four wide column">
-          <h1>{props.fighter.name}</h1>
-          <p>Age: {props.fighter.age}</p>
+          <h1>{fighter.name}</h1>
+          <p>Age: {fighter.age}</p>
           <p>Weight Class: {props.fighter.weight_class}</p>
           <p>Hometown: {props.fighter.hometown}</p>
           <p>Gym: <Link style={{ marginBottom: '5px'}} to={`/gyms/${props.fighter.gym.id}`}>{props.fighter.gym.name}</Link></p>
@@ -17,6 +29,14 @@ const FighterDetails = (props) => {
         </div>
         <div className="four wide column">
           <img src={props.fighter.image_url} />
+        </div>
+        <div>
+          <WrappedLink props={props} />
+        </div>
+        <div>
+          <Switch>
+            <Route exact path={`${match.url}/:fighterId/edit`} component={(props) => <FighterEdit {...props} fighter={fighter} />}  />
+          </Switch>
         </div>
       </div>
     </div>
