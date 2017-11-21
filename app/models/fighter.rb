68d1f603192791f6afc04c54133953b3119b4e6c
dustin <dandersonsoftware@outlook.com>
2017-11-21 07:48:51 -0500

    added validation to back end

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,3 +1,10 @@
 class Fighter < ApplicationRecord
   belongs_to :gym
+  validates :name, presence: true
+  validates :age, numericality: true
+  validates :weight_class, presence: true
+  validates :hometown,presence: true
+  validates :gym_id, presence: true, numericality: true
+  validates :description, presence: true
+  validates :image_url, presence: true
 end
--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,3 +1,8 @@
 class Gym < ApplicationRecord
   has_many :fighters
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :owner, presence: true
+  validates :image_url, presence: true
+  validates :description, presence: true
 end
