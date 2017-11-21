61c692023ed5ce62224175dff52bee540230781f
dustin <dandersonsoftware@outlook.com>
2017-11-21 07:16:46 -0500

    styling

--- client/src/components/fighters/FighterForm.js
+++ client/src/components/fighters/FighterForm.js
@@ -59,82 +59,88 @@ class FighterForm extends Component {
           <h1>Add a New Fighter</h1>
           <div className="ui one column stackable center aligned grid container">
             <div className="center aligned column twelve wide">
+              <div className="fields">
+                <div className="eight wide field">
+                  <label>Name: </label>
+                  <input
+                    type='text'
+                    name="name"
+                    onChange={this.handleOnChange}
+                    value={this.state.name}
+                   />
+                </div>
 
-              <div className="eight wide field">
-                <label>Name: </label>
-                <input
-                  type='text'
-                  name="name"
-                  onChange={this.handleOnChange}
-                  value={this.state.name}
-                 />
+                <div className="eight wide field">
+                  <label>Age: </label>
+                  <input
+                    type='number'
+                    name="age"
+                    min='18'
+                    max='99'
+                    onChange={this.handleOnChange}
+                    value={this.state.age}
+                    />
+                </div>
               </div>
 
-              <div className="eight wide field">
-                <label>Age: </label>
-                <input
-                  type='number'
-                  name="age"
-                  min='18'
-                  max='99'
-                  onChange={this.handleOnChange}
-                  value={this.state.age}
+              <div className="fields">
+                <div className="eight wide field">
+                  <label>Weight Class </label>
+                  <select
+                    name="weight_class"
+                    onChange={this.handleOnChange}
+                    value={this.state.weight_class}>
+                    <option value="Flyweight">Flyweight</option>
+                    <option value="Bantamweight">Bantamweight</option>
+                    <option value="Featherweight">Featherweight</option>
+                    <option value="Lightweight">Lightweight</option>
+                    <option value="Welterweight">Welterweight</option>
+                    <option value="Middleweight">Middleweight</option>
+                    <option value="Light Heavyweight">Light Heavyweight</option>
+                    <option value="Heavyweight">Heavyweight</option>
+                  </select>
+                </div>
+
+                <div className="eight wide field">
+                  <label>Hometown </label>
+                  <input
+                    type="text"
+                    name="hometown"
+                    onChange={this.handleOnChange}
+                    value={this.state.hometown}
                   />
+                </div>
               </div>
 
-              <div className="eight wide field">
-                <label>Weight Class </label>
-                <select
-                  name="weight_class"
-                  onChange={this.handleOnChange}
-                  value={this.state.weight_class}>
-                  <option value="Flyweight">Flyweight</option>
-                  <option value="Bantamweight">Bantamweight</option>
-                  <option value="Featherweight">Featherweight</option>
-                  <option value="Lightweight">Lightweight</option>
-                  <option value="Welterweight">Welterweight</option>
-                  <option value="Middleweight">Middleweight</option>
-                  <option value="Light Heavyweight">Light Heavyweight</option>
-                  <option value="Heavyweight">Heavyweight</option>
-                </select>
+              <div className="fields">
+                <div className="eight wide field">
+                  <label>Image URL</label>
+                  <input
+                    type="text"
+                    name="image_url"
+                    onChange={this.handleOnChange}
+                    value={this.state.image_url}
+                  />
+                </div>
+
+                <div className="eight wide field">
+                  <label>Fighter Gym: </label>
+                    <FighterFormSelectOptions gyms={this.props.gyms} />
+                </div>
               </div>
 
-              <div className="eight wide field">
-                <label>Hometown </label>
-                <input
-                  type="text"
-                  name="hometown"
-                  onChange={this.handleOnChange}
-                  value={this.state.hometown}
-                />
-              </div>
+                <div className="eight wide field">
+                  <label>Description </label>
+                  <textarea
+                    type="text"
+                    name="description"
+                    onChange={this.handleOnChange}
+                    value={this.state.description}
+                  />
+                </div>
 
-              <div className="eight wide field">
-                <label>Image URL</label>
-                <input
-                  type="text"
-                  name="image_url"
-                  onChange={this.handleOnChange}
-                  value={this.state.image_url}
-                />
-              </div>
+                <input type="submit" value="Submit New Fighter" />
 
-              <div className="eight wide field">
-                <label>Fighter Gym: </label>
-                  <FighterFormSelectOptions gyms={this.props.gyms} />
-              </div>
-
-              <div className="eight wide field">
-                <label>Description </label>
-                <textarea
-                  type="text"
-                  name="description"
-                  onChange={this.handleOnChange}
-                  value={this.state.description}
-                />
-              </div>
-
-              <input type="submit" value="Submit New Fighter" />
             </div>
           </div>
         </div>
